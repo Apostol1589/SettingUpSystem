ec13d60446f460a27c77aeab0b03072be3c8fb96
ruslan <nimdabs234@gmail.com>
2025-05-29 16:34:25 +0300

    Added user creation class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+
+            string username = $"Te$t{r.Next(666)}";
+            string password = username;
+
+            string startProgramPath = "\"C:\\Program Files\\BAF\\common\\1cestart.exe\" enterprise /F\"D:\\B\"";
+            string starInPath = "C:\\Program Files\\BAF\\common\\";
+
+            UserCreation.CreateUser_Kasa(username, password, startProgramPath, starInPath);
+
+            //UserCreation.CreateUser(username, password);
+
+
+            Console.ReadLine();
         }
+
     }
 }
---
+++ UserCreation.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+using Microsoft.Win32;
+
+namespace SettingUpSystem
+{
+    internal class UserCreation
+    { 
+        public static void CreateUser_Kasa(string username, string password, string startProgramPath, string startInPath)
+        {
+            try
+            {
+                Process.Start(
+                    new ProcessStartInfo
+                    {
+                        FileName = "net",
+                        Arguments = $"user {username} {password} /add",
+                        Verb = "runas",
+                        UseShellExecute = true
+                    }).WaitForExit();
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "net",
+                    Arguments = $"localgroup \"Remote Desktop Users\" {username} /add",
+                    Verb = "runas",
+                    UseShellExecute = true
+                }).WaitForExit();
+
+                Console.WriteLine($"User {username} created");
+
+                string userRegistryPath = $"HKEY_USERS\\{GetUserSID(username)}\\Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+                Registry.SetValue(userRegistryPath, "Start1C", startProgramPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+
+        static string GetUserSID(string username)
+        {
+            var ntAccount = new NTAccount(username);
+            var sid = (SecurityIdentifier)ntAccount.Translate(typeof(SecurityIdentifier));
+            return sid.Value;
+        }
+
+        public static void CreateUser_simple(string username, string password)
+        {
+            string command = $"net user {username} {password} /add /passwordchg:no";
+
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe", "/c" + command)
+            {
+                Verb = "runas",
+                UseShellExecute = true,
+            };
+
+            try
+            {
+                Process process = Process.Start(psi);
+                process.WaitForExit();
+
+                Console.WriteLine("User created successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: " + ex.Message);
+            }
+        }
+
+    }
+}
