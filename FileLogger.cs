3a3e890f56943332205ea313e029c5f4aac1b7a6
ruslan <nimdabs234@gmail.com>
2025-06-06 15:57:22 +0300

    Added simple structure and program installing classes

---
+++ FileLogger.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace SettingUpSystem
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class FileLogger : ILogger
+    {
+        private readonly string _logPath;
+
+        public FileLogger(string logPath)
+        {
+            _logPath = logPath;
+        }
+
+        public void Log(string message)
+        {
+            string entry = $"[{DateTime.Now:HH:mm:ss}] {message}";
+            File.AppendAllLines(_logPath, new[] { entry });
+            Console.WriteLine(entry);
+        }
+
+    }
+}
---
+++ SetupTask/ProgramInstallationTask.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using SettingUpSystem.Services;
+
+namespace SettingUpSystem.SetupTask
+{
+    internal class ProgramInstallationTask
+    {
+        public string Name => "Program installing";
+
+        private readonly ProcessService _processService;
+
+        public ProgramInstallationTask(ProcessService processService)
+        {
+            _processService = processService;
+        }
+
+        public async Task<bool> ExecuteAsync(ILogger logger)
+        {
+            string basePath = @"D:\(01) Soft_For_Shop";
+
+            var installers = new List<(string fileName, string arguments)>
+            {
+            ("AnyDesk_6_2_3_0.exe", "/silent"),
+            ("ChromeSetup.exe", "/silent /install"),
+            ("LibreOffice_7.6.7_Win_x86-64.msi", "/quiet"),
+            ("winrar-x64-701uk.exe", "/S")
+            };
+
+            bool allSucceeded = true;
+
+            foreach (var (fileName, args) in installers)
+            {
+                string fullPath = Path.Combine(basePath, fileName);
+                logger.Log($"Installing {fileName}...");
+
+                if (!File.Exists(fullPath))
+                {
+                    logger.Log($"File not found {fullPath}");
+                    allSucceeded = false;
+                    continue;
+                }
+
+                bool result = _processService.RunSilentInstaller(fullPath, args);
+
+                if (result)
+                    logger.Log($"Success installed {fileName}");
+                else
+                {
+                    logger.Log($"Error with {fileName}");
+                    allSucceeded = false;
+                }
+            }
+            return allSucceeded;
+        }
+    }
+}
